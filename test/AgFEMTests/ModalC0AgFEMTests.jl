0481d0221734e3f2fed53895628ac87923ea0254
eneiva <eneiva@cimne.upc.edu>
2021-02-25 11:08:09 +0100

    Added Julia script to run experiments in MPI

--- test/AgFEMTests/ModalC0AgFEMTests.jl
+++ test/AgFEMTests/ModalC0AgFEMTests.jl
@@ -1,196 +1,204 @@
-using DrWatson
-@quickactivate "GridapEmbedded"
+module ModalC0AgFEMTests
 
-using Gridap
-using GridapEmbedded
-using GridapEmbedded.AgFEM
+  using DrWatson
+  @quickactivate "GridapEmbedded"
 
-using LinearAlgebra: cond
-using SparseArrays: SparseMatrixCSC
+  using Gridap
+  using GridapEmbedded
+  using GridapEmbedded.AgFEM
 
-using GridapPETSc
-using MPI
+  using LinearAlgebra: cond
+  using SparseArrays: SparseMatrixCSC
 
-if !MPI.Initialized()
-  MPI.Init()
-end
+  using GridapPETSc
+  using MPI
 
-tol = 1e-16
-maxits = 10000
-GridapPETSc.Init(["-ksp_type", "cg",
-                  "-ksp_rtol", "$tol",
-                  "-ksp_max_it", "$maxits",
-                  "-ksp_norm_type", "unpreconditioned",
-                  "-pc_type","gamg",
-                  "-pc_gamg_type","agg",
-                  "-pc_gamg_esteig_ksp_type","cg",
-                  "-mg_levels_esteig_ksp_type","cg",
-                  "-mg_coarse_sub_pc_type","cholesky",
-                  "-mg_coarse_sub_pc_factor_mat_ordering_type","nd",
-                  "-pc_gamg_process_eq_limit","50",
-                  "-pc_gamg_square_graph","0",
-                  "-pc_gamg_agg_nsmooths","1"])
+  const tol = 1e-16
+  const maxits = 10000
+  const R = 0.42
+  const ω = 2*pi
+  exact(x) = x[1]+x[2]
+  sinus(x) = sin(ω*x[1])*x[2]
 
-const R = 0.42
-const ω = 2*pi
-exact(x) = x[1]+x[2]
-sinus(x) = sin(ω*x[1])*x[2]
+  function compute(n::Int,k::Int,d::Int,t::Int,s::Int,g::Int)
 
-function run(n::Int,k::Int,d::Int,t::Int,s::Int,g::Int)
+    u(x) = (1-s)*exact(x)+s*sinus(x)
+    f(x) = -Δ(u)(x)
+    ud(x) = u(x)
 
-  u(x) = (1-s)*exact(x)+s*sinus(x)
-  f(x) = -Δ(u)(x)
-  ud(x) = u(x)
-
-  if d == 2
-    if g == 0
-      geom = disk(R,x0=Point(0.5,0.5))
+    if d == 2
+      if g == 0
+        geom = disk(R,x0=Point(0.5,0.5))
+      else
+        geom = square(L=0.63,x0=Point(0.5,0.5))
+      end
+      partition = (n,n)
+      domain = (0,1,0,1)
     else
-      geom = square(L=0.63,x0=Point(0.5,0.5))
+      if g == 0
+        geom = sphere(R,x0=Point(0.5,0.5,0.5))
+      else
+        geom = doughnut(0.21,0.1,x0=Point(0.5,0.5,0.5))
+      end
+      partition = (n,n,n)
+      domain = (0,1,0,1,0,1)
     end
-    partition = (n,n)
-    domain = (0,1,0,1)
-  else
-    if g == 0
-      geom = sphere(R,x0=Point(0.5,0.5,0.5))
+
+    bgmodel = CartesianDiscreteModel(domain,partition)
+    h = (domain[2]-domain[1])/n
+    cutgeo = cut(bgmodel,geom)
+    model = DiscreteModel(cutgeo)
+
+    if t == 0
+      γ₀ = 0.65
+      eᵧ = (3-2/num_dims(model))/(2*k+1-2/num_dims(model))
+      γ = γ₀^eᵧ
+      strategy = AggregateCutCellsByThreshold(γ)
     else
-      geom = doughnut(0.21,0.1,x0=Point(0.5,0.5,0.5))
+      strategy = AggregateAllCutCells()
     end
-    partition = (n,n,n)
-    domain = (0,1,0,1,0,1)
-  end
 
-  bgmodel = CartesianDiscreteModel(domain,partition)
-  h = (domain[2]-domain[1])/n
-  cutgeo = cut(bgmodel,geom)
-  model = DiscreteModel(cutgeo)
+    aggregates = aggregate(strategy,cutgeo,geom)
+    bboxes = compute_cell_to_dface_bboxes(model,aggregates)
 
-  if t == 0
-    γ₀ = 0.65
-    eᵧ = (3-2/num_dims(model))/(2*k+1-2/num_dims(model))
-    γ = γ₀^eᵧ
-    strategy = AggregateCutCellsByThreshold(γ)
-  else
-    strategy = AggregateAllCutCells()
-  end
+    Ω_bg = Triangulation(bgmodel)
+    Ω = Triangulation(cutgeo)
+    Γ = EmbeddedBoundary(cutgeo)
 
-  aggregates = aggregate(strategy,cutgeo,geom)
-  bboxes = compute_cell_to_dface_bboxes(model,aggregates)
+    n_Γ = get_normal_vector(Γ)
 
-  Ω_bg = Triangulation(bgmodel)
-  Ω = Triangulation(cutgeo)
-  Γ = EmbeddedBoundary(cutgeo)
+    D = num_dims(model)
+    cutdeg, degree = 2*D*k, 2*k
+    dΩ = Measure(Ω,cutdeg,degree)
+    dΓ = Measure(Γ,cutdeg)
 
-  n_Γ = get_normal_vector(Γ)
+    γd = 5.0*k^2
 
-  D = num_dims(model)
-  cutdeg, degree = 2*D*k, 2*k
-  dΩ = Measure(Ω,cutdeg,degree)
-  dΓ = Measure(Γ,cutdeg)
+    a(u,v) =
+      ∫( ∇(u)⋅∇(v) ) * dΩ +
+      ∫( (γd/h)*u*v  - v*(n_Γ⋅∇(u)) - (n_Γ⋅∇(v))*u ) * dΓ
 
-  γd = 5.0*k^2
+    l(v) =
+      ∫( v*f ) * dΩ +
+      ∫( (γd/h)*v*ud - (n_Γ⋅∇(v))*ud ) * dΓ
 
-  a(u,v) =
-    ∫( ∇(u)⋅∇(v) ) * dΩ +
-    ∫( (γd/h)*u*v  - v*(n_Γ⋅∇(u)) - (n_Γ⋅∇(v))*u ) * dΓ
+    l2(u) = sqrt(sum( ∫( u*u )*dΩ ))
+    h1(u) = sqrt(sum( ∫( u*u + ∇(u)⋅∇(u) )*dΩ ))
 
-  l(v) =
-    ∫( v*f ) * dΩ +
-    ∫( (γd/h)*v*ud - (n_Γ⋅∇(v))*ud ) * dΓ
-
-  l2(u) = sqrt(sum( ∫( u*u )*dΩ ))
-  h1(u) = sqrt(sum( ∫( u*u + ∇(u)⋅∇(u) )*dΩ ))
-
-  reffe = ReferenceFE(modalC0,Float64,k,bboxes)
-  Vstd = TestFESpace(model,reffe,conformity=:H1)
-  V = AgFEMSpace(Vstd,aggregates,modalC0)
-  U = TrialFESpace(V)
-
-  ass = SparseMatrixAssembler(SparseMatrixCSR{0,PetscReal,PetscInt},U,V)
-  op = AffineFEOperator(a,l,ass)
-  A = convert(SparseMatrixCSC, get_matrix(op))
-  kopM = cond(A,1) # https://github.com/JuliaLang/julia/issues/6485
-
-  ls = PETScSolver()
-  solver = LinearFESolver(ls)
-
-  uh = solve(solver,op)
-  iteM = PETSc_get_number_of_iterations(ls)
-
-  e = u - uh
-  el2M = l2(e)
-  eh1M = h1(e)
-
-  if k != 1
-    reffe = ReferenceFE(lagrangian,Float64,k)
+    reffe = ReferenceFE(modalC0,Float64,k,bboxes)
     Vstd = TestFESpace(model,reffe,conformity=:H1)
-    V = AgFEMSpace(Vstd,aggregates,lagrangian)
+    V = AgFEMSpace(Vstd,aggregates,modalC0)
     U = TrialFESpace(V)
 
     ass = SparseMatrixAssembler(SparseMatrixCSR{0,PetscReal,PetscInt},U,V)
     op = AffineFEOperator(a,l,ass)
     A = convert(SparseMatrixCSC, get_matrix(op))
-    kopN = cond(A,1) # https://github.com/JuliaLang/julia/issues/6485
+    kopM = cond(A,1) # https://github.com/JuliaLang/julia/issues/6485
 
     ls = PETScSolver()
     solver = LinearFESolver(ls)
 
     uh = solve(solver,op)
-    iteN = PETSc_get_number_of_iterations(ls)
+    iteM = PETSc_get_number_of_iterations(ls)
 
     e = u - uh
-    el2N = l2(e)
-    eh1N = h1(e)
-  else
-    el2N = el2M
-    eh1N = eh1M
-    kopN = kopM
-    iteN = iteM
+    el2M = l2(e)
+    eh1M = h1(e)
+
+    if k != 1
+      reffe = ReferenceFE(lagrangian,Float64,k)
+      Vstd = TestFESpace(model,reffe,conformity=:H1)
+      V = AgFEMSpace(Vstd,aggregates,lagrangian)
+      U = TrialFESpace(V)
+
+      ass = SparseMatrixAssembler(SparseMatrixCSR{0,PetscReal,PetscInt},U,V)
+      op = AffineFEOperator(a,l,ass)
+      A = convert(SparseMatrixCSC, get_matrix(op))
+      kopN = cond(A,1) # https://github.com/JuliaLang/julia/issues/6485
+
+      ls = PETScSolver()
+      solver = LinearFESolver(ls)
+
+      uh = solve(solver,op)
+      iteN = PETSc_get_number_of_iterations(ls)
+
+      e = u - uh
+      el2N = l2(e)
+      eh1N = h1(e)
+    else
+      el2N = el2M
+      eh1N = eh1M
+      kopN = kopM
+      iteN = iteM
+    end
+
+    num_free_dofs(U)^(1/D), el2M, eh1M, kopM, iteM, el2N, eh1N, kopN, iteN
+
   end
 
-  num_free_dofs(U)^(1/D), el2M, eh1M, kopM, iteM, el2N, eh1N, kopN, iteN
+  function compute(case::Dict)
+    @unpack n,k,d,t,s,g = case
+    udofs, el2M, eh1M, kopM, iteM, el2N, eh1N, kopN, iteN = compute(n,k,d,t,s,g)
+    results = @dict udofs el2M eh1M kopM iteM el2N eh1N kopN iteN
+    merge(case,results)
+  end
 
-end
+  function compute_and_save(case::Dict)
+    produce_or_load(
+      datadir(),
+      case,
+      compute,
+      prefix="modalC0",
+      tag=true,
+      verbose=true
+    )
+    return true
+  end
 
-function run(case::Dict)
-  @unpack n,k,d,t,s,g = case
-  udofs, el2M, eh1M, kopM, iteM, el2N, eh1N, kopN, iteN = run(n,k,d,t,s,g)
-  results = @dict udofs el2M eh1M kopM iteM el2N eh1N kopN iteN
-  merge(case,results)
-end
+  function compute()
 
-function run_and_save(case::Dict)
-  produce_or_load(
-    datadir(),
-    case,
-    run,
-    prefix="modalC0",
-    tag=true,
-    verbose=true
-  )
-  return true
-end
+    if !MPI.Initialized()
+      MPI.Init()
+    end
 
-function run()
-  params = Dict(
-    :n => [12,24],
-    :k => [1,2],
-    :d => [2,3],
-    :t => [0,1],
-    :s => [0,1],
-    :g => [0,1]
-  )
-  dicts = dict_list(params)
-  map(run_and_save,dicts)
-end
+    GridapPETSc.Init(["-ksp_type", "cg",
+                      "-ksp_rtol", "$tol",
+                      "-ksp_max_it", "$maxits",
+                      "-ksp_norm_type", "unpreconditioned",
+                      "-pc_type","gamg",
+                      "-pc_gamg_type","agg",
+                      "-pc_gamg_esteig_ksp_type","cg",
+                      "-mg_levels_esteig_ksp_type","cg",
+                      "-mg_coarse_sub_pc_type","cholesky",
+                      "-mg_coarse_sub_pc_factor_mat_ordering_type","nd",
+                      "-pc_gamg_process_eq_limit","50",
+                      "-pc_gamg_square_graph","0",
+                      "-pc_gamg_agg_nsmooths","1"])
 
-@info "Training"
-run(6,1,3,1,0,0)
-@info "Producing"
-run()
+    params = Dict(
+      :n => [12],
+      :k => [1,2],
+      :d => [2,3],
+      :t => [0,1],
+      :s => [0,1],
+      :g => [0,1]
+    )
+    dicts = dict_list(params)
+    map(compute_and_save,dicts)
 
-GridapPETSc.Finalize()
+    GridapPETSc.Finalize()
 
-if MPI.Initialized() & !MPI.Finalized()
-  MPI.Finalize()
+    if MPI.Initialized() & !MPI.Finalized()
+      MPI.Finalize()
+    end
+
+  end
+
+  # @info "Training"
+  # compute(6,1,3,1,0,0)
+  # @info "Producing"
+  # compute()
+  export tol, maxits
+  export compute, compute_and_save
+
 end
---
+++ test/AgFEMTests/RunModalC0AgFEMTests.jl
@@ -0,0 +1,48 @@
+using MPIClusterManagers, Distributed
+import MPI
+
+manager = MPIClusterManagers.start_main_loop(MPI_TRANSPORT_ALL)
+
+@everywhere function pidandhost()
+  host, pid = (gethostname(), getpid())
+  println("Hello from process $(pid) on host $(host)!")
+end
+
+@everywhere include("test/AgFEMTests/ModalC0AgFEMTests.jl")
+@everywhere using .ModalC0AgFEMTests
+@everywhere using DrWatson
+@everywhere @quickactivate "GridapEmbedded"
+@everywhere using GridapPETSc
+@everywhere GridapPETSc.Init(["-ksp_type", "cg",
+                              "-ksp_rtol", "$tol",
+                              "-ksp_max_it", "$maxits",
+                              "-ksp_norm_type", "unpreconditioned",
+                              "-pc_type","gamg",
+                              "-pc_gamg_type","agg",
+                              "-pc_gamg_esteig_ksp_type","cg",
+                              "-mg_levels_esteig_ksp_type","cg",
+                              "-mg_coarse_sub_pc_type","cholesky",
+                              "-mg_coarse_sub_pc_factor_mat_ordering_type","nd",
+                              "-pc_gamg_process_eq_limit","50",
+                              "-pc_gamg_square_graph","0",
+                              "-pc_gamg_agg_nsmooths","1"])
+
+params = Dict(
+  :n => [6],
+  :k => [1,2],
+  :d => [2],
+  :t => [0,1],
+  :s => [0,1],
+  :g => [0,1]
+)
+dicts = dict_list(params)
+
+@everywhere pidandhost()
+@everywhere @info "Training"
+@everywhere compute(6,1,3,1,0,0)
+@everywhere @info "Producing"
+pmap(compute_and_save,dicts)
+
+@everywhere GridapPETSc.Finalize()
+
+MPIClusterManagers.stop_main_loop(manager)
